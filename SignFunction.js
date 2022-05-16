377641ac1b2e87459a170ef73183391b58df6ff3
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-05-16 15:23:34 +0100

    1t Stage

---
+++ SignFunction.js
@@ -0,0 +1,19 @@
+const Panel = document.querySelector("#Panel_Signs")
+const Buttons = document.querySelectorAll(".button")
+const Plane_Image = document.querySelector("#Plane_Image")
+
+Buttons.forEach( button => button.addEventListener("click", (event) => {
+  // importante this.id
+
+  
+  console.log('asdsadasdas');
+  
+   Panel.setAttribute("text", `value:  ${button.id} ;  color:  #000000 ;width: 3`);
+   
+  
+
+   Plane_Image.setAttribute("material",`src:/ImagensSigns/${button.id}.jpg`)
+   
+}))
+
+ console.log(Panel);
