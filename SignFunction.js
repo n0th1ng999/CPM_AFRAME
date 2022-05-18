827d114c18efa877eff991ddb817e874ca66c581
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-05-18 17:01:13 +0100

    Videos
    ya videos

--- SignFunction.js
+++ SignFunction.js
@@ -1,19 +1,20 @@
-const Panel = document.querySelector("#Panel_Signs")
+
 const Buttons = document.querySelectorAll(".button")
-const Plane_Image = document.querySelector("#Plane_Image")
+const Panels = document.querySelectorAll(".panel")
+
+
 
 Buttons.forEach( button => button.addEventListener("click", (event) => {
   // importante this.id
 
   
-  console.log('asdsadasdas');
   
-   Panel.setAttribute("text", `value:  ${button.id} ;  color:  #fff ;width: 3`);
    
+  Panels.forEach(Panel => Panel.setAttribute("src",`#aslVID_${button.id}`) )
   
 
-   Plane_Image.setAttribute("material",`src:/ImagensSigns/${button.id}.jpg`)
+  
    
 }))
 
- console.log(Panel);
+
