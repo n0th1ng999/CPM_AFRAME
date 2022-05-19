2527c69f4209c5935de14b28fb88ca4e7e9ab7f0
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-05-19 15:08:15 +0100

    Done i think

--- SignFunction.js
+++ SignFunction.js
@@ -1,19 +1,9 @@
-
 const Buttons = document.querySelectorAll(".button")
 const Panels = document.querySelectorAll(".panel")
 
-
-
 Buttons.forEach( button => button.addEventListener("click", (event) => {
-  // importante this.id
-
-  
-  
    
-  Panels.forEach(Panel => Panel.setAttribute("src",`#aslVID_${button.id}`) )
-  
-
-  
+  Panels.forEach(Panel => Panel.setAttribute("src",`#aslVID_${button.id}`) )  
    
 }))
 
